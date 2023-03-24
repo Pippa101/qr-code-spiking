f95d97ad973d68482c48b030710ecab5875812f4
Pippa101 <94748151+Pippa101@users.noreply.github.com>
2023-03-24 15:45:57 +0000

    styling video

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ function App() {
     <div className="App">
       <h1>QR Spike!</h1>
       <QRCode size={200} bgColor="white" fgColor="black" value="1234" />
-      <QrScanner
-        className="scanner"
-        constraints={{ facingMode: "environment" }}
-        onDecode={(result) => console.log(result)}
-        onError={(error) => console.log(error?.message)}
-      />
+      <div id="scanner-container">
+        <QrScanner
+          videoStyle={{
+            objectFit: "cover",
+          }}
+          className="scanner"
+          constraints={{ facingMode: "environment" }}
+          onDecode={(result) => console.log(result)}
+          onError={(error) => console.log(error?.message)}
+        />
+      </div>
     </div>
   );
 }
